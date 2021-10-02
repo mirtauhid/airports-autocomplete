d9b55543b83746f19496acbf9bf8b43d1230109e
MIR TAUHIDUL ISLAM <dev.tauhidul@gmail.com>
2021-10-03 02:01:31 +0600

    fix 1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+module.exports = async function getAirports() {
+  const axios = require('axios');
 
+  try {
+    const airports = await axios.get(
+      'https://raw.githubusercontent.com/algolia/datasets/master/airports/airports.json'
+    );
+    return airports;
+  } catch (error) {
+    console.log(error);
+  }
+
+  return airports;
+};
